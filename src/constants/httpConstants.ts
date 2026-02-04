fac6ddaa58be43075cf88cb521ea48af5d0eccc8
David Nesca <Dnesca@rrc.ca>
2026-02-03 23:16:43 -0600

    Constants establish according to the assignment

---
+++ src/constants/httpConstants.ts
@@ -0,0 +1,19 @@
+/**
+ */
+export const HTTP_STATUS = {
+    // Success responses
+    OK: 200,
+    CREATED: 201,
+
+    // Client error responses
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+
+    // Server error responses
+    INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+/**
+ * Allowed HTTP status code values from HTTP_STATUS.
+ */
+export type HttpStatusCode = (typeof HTTP_STATUS)[keyof typeof HTTP_STATUS];
