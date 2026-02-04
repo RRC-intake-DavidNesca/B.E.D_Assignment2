212bf9cbda1e34c8d1584c55c4acc2cc346093f0
David Nesca <Dnesca@rrc.ca>
2026-02-03 22:41:44 -0600

    add morgan http request logging

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 import v1Router from "./api/v1/routes";
 
 const app: Express = express();
+
+app.use(morgan("combined"));
 
 app.use(express.json());
 
